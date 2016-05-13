c88094cc91a8cc4326255c79eb60d4a840e71f4e
Angus McLean <angus.mclean@mail.mcgill.ca>
2016-05-12 21:09:44 -0400

    Adjusts how it works section, centers facebook images better, implements hover effect

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,19 @@
 // grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload');
 
 gulp.task('connect', function () {
   connect.server();
 })
 
+gulp.task('watch', function(){
+  livereload.listen();
+  gulp.watch('index.html');
+  gulp.watch('js/');
+  gulp.watch('css/');
+})
 
 // create a default task and just log a message
-gulp.task('default', ['connect']);
+gulp.task('default', ['connect', 'watch']);
